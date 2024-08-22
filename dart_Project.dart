6cc87e2a410ff6d0d80b29360a73a30a9cc4ba50
Code Faizi Code <codefaizicode@gmail.com>
2024-08-22 15:43:30 -0500

    update

---
+++ dart_Project.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+List<String> stdName = List.filled(20, '');
+List<String> stdRoll = List.filled(20, '');
+List<String> stdSubj = List.filled(20, '');
+List<String> stdCont = List.filled(20, '');
+int total = 0;
