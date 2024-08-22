15a22bbf4d04124507596def859dfb762a071454
Code Faizi Code <codefaizicode@gmail.com>
2024-08-22 15:44:33 -0500

    update

--- dart_Project.dart
+++ dart_Project.dart
@@ -6,7 +6,6 @@ List<String> stdSubj = List.filled(20, '');
 List<String> stdCont = List.filled(20, '');
 int total = 0;
 
-
 void enter() {
   stdout.write("\n\n          HOW MANY STUDENTS YOU WANT TO ENTER?? : ");
   int choice = int.parse(stdin.readLineSync()!);
@@ -24,3 +23,21 @@ void enter() {
       stdout.write("          Enter Contact  :  ");
       stdCont[i] = stdin.readLineSync()!;
     }
+
+    print("");
+  } else {
+    for (int i = total; i < total + choice; i++) {
+      stdout.write("\n          ENTER THE DATA OF STUDENT ${i + 1}");
+      stdout.write("          ***************************\n\n");
+      stdout.write("          Enter Name     :  ");
+      stdName[i] = stdin.readLineSync()!;
+      stdout.write("          Enter Roll no  :  ");
+      stdRoll[i] = stdin.readLineSync()!;
+      stdout.write("          Enter Subject  :  ");
+      stdSubj[i] = stdin.readLineSync()!;
+      stdout.write("          Enter Contact  :  ");
+      stdCont[i] = stdin.readLineSync()!;
+    }
+    total = choice + total;
+  }
+}
